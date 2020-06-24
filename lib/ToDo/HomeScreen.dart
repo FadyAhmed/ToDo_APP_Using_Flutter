350d02953a4c3333af40853e43549b95eb4f08c5
Fady Ahmed <fady.999666@gmail.com>
2020-06-25 01:09:26 +0200

    it's ok

--- lib/ToDo/HomeScreen.dart
+++ lib/ToDo/HomeScreen.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
+import 'package:TODO_APP/ToDo/NewTask.dart';
 import 'package:TODO_APP/auth/login.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -8,10 +12,31 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ScrollController scrollController = ScrollController();
+  String done = 'true';
+  bool _bottom;
+  IconData box;
+  void initState() {
+    super.initState();
+    scrollController.addListener(() {
+      if (scrollController.position.maxScrollExtent ==
+          scrollController.position.pixels) {
+        setState(() {
+          _bottom = true;
+        });
+      } else {
+        setState(() {
+          _bottom = false;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('Tasks'),
         actions: <Widget>[
           FlatButton(
             child: Text('Logout'),
@@ -23,6 +48,183 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) => NewTask()));
+        },
+        child: Icon(Icons.add),
+      ),
+      body: StreamBuilder(
+          stream: Firestore.instance
+              .collection('Tasks')
+              .orderBy('date', descending: true)
+              .snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return _loading();
+                break;
+
+              case ConnectionState.none:
+                return internetConnectionPeoblem();
+                break;
+              case ConnectionState.done:
+                if (snapshot.error != null) {
+                  return internetConnectionPeoblem();
+                } else {
+                  if (snapshot.hasData) {
+                    _body(context, snapshot.data, _bottom);
+                  } else {
+                    return _noData();
+                  }
+                }
+                break;
+            }
+
+            return _body(context, snapshot.data, _bottom);
+          }),
     );
+  }
+
+  Widget _body(BuildContext context, QuerySnapshot data, bool bottom) {
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
+        _bottom = true;
+      } else {
+        _bottom = false;
+      }
+    });
+    print(bottom);
+
+    if (data.documents.length == 0) {
+      return _noData();
+    } else {
+      return ListView.builder(
+          controller: scrollController,
+          itemCount: data.documents.length,
+          itemBuilder: (BuildContext context, int index) {
+            done = data.documents[index]['done'].toString();
+
+            if (done == 'true') {
+              box = Icons.check_box;
+            } else {
+              box = Icons.check_box_outline_blank;
+            }
+            return Padding(
+              padding: const EdgeInsets.only(top: 8, left: 8, right: 8),
+              child: Card(
+                elevation: 3,
+                child: ExpansionTile(
+                  leading: Transform.translate(
+                    offset: Offset(-10, 0),
+                    child: IconButton(
+                      icon: Icon(box),
+                      onPressed: () {
+                        done = data.documents[index]['done'].toString();
+
+                        if (done == 'true') {
+                          done = 'false';
+                          Firestore.instance
+                              .collection('Tasks')
+                              .document(data.documents[index].documentID)
+                              .updateData({'done': done});
+                        } else {
+                          done = 'true';
+                          Firestore.instance
+                              .collection('Tasks')
+                              .document(data.documents[index].documentID)
+                              .updateData({'done': done});
+                        }
+                      },
+                    ),
+                  ),
+                  title: Transform.translate(
+                      offset: Offset(-15, 0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                              child: Text(
+                                  data.documents[index]['body'].toString())),
+                        ],
+                      )),
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 24.0, bottom: 4, right: 24),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(data.documents[index]['done'].toString()),
+                      ),
+                    ),
+                  ],
+                  trailing: Wrap(
+                    spacing: -15,
+                    children: <Widget>[
+                      IconButton(icon: Icon(Icons.edit), onPressed: () {}),
+                      IconButton(
+                          icon: Icon(Icons.offline_pin), onPressed: () {}),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          });
+    }
+  }
+
+  Widget _loading() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _noData() {
+    return Center(child: Text('No Tasks ToDo yet'));
+  }
+}
+
+Widget internetConnectionPeoblem() {
+  return Center(
+    child: Column(
+      children: <Widget>[
+        Text('Internet connection problem..'),
+        RaisedButton(
+          onPressed: () {},
+          child: Text('Refresh'),
+        )
+      ],
+    ),
+  );
+}
+
+class CheckBox extends StatefulWidget {
+  CheckBox(
+    this.done,
+    this.index,
+    this.data,
+  );
+  bool done;
+  int index;
+  QuerySnapshot data;
+  @override
+  CheckBoxState createState() => CheckBoxState();
+}
+
+class CheckBoxState extends State<CheckBox> {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+        icon: widget.done
+            ? Icon(Icons.check_box_outline_blank)
+            : Icon(Icons.check_box),
+        onPressed: () {
+          widget.done = !widget.done;
+          Firestore.instance
+              .collection('Tasks')
+              .document(widget.data.documents[widget.index].documentID)
+              .updateData({'done': widget.done});
+        });
   }
 }
---
+++ lib/ToDo/NewTask.dart
@@ -0,0 +1,73 @@
+import 'package:TODO_APP/ToDo/HomeScreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class NewTask extends StatefulWidget {
+  @override
+  _NewTaskState createState() => _NewTaskState();
+}
+
+class _NewTaskState extends State<NewTask> {
+  var _formKey = GlobalKey<FormState>();
+  bool _done = false;
+  bool _autoValidate = true;
+  TextEditingController _taskController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.save),
+          onPressed: _sumbit,
+        ),
+        appBar: AppBar(
+          title: Text('New Task'),
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: EdgeInsets.all(12),
+          child: Form(
+            autovalidate: _autoValidate,
+            key: _formKey,
+            child: TextFormField(
+              focusNode: FocusNode(canRequestFocus: true),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Nothing ToDo';
+                } else {
+                  return null;
+                }
+              },
+              controller: _taskController,
+              decoration: InputDecoration(
+                
+                hintText: '  ToDo',
+              ),
+            ),
+          ),
+        ));
+  }
+
+  void _sumbit() {
+    if (!_formKey.currentState.validate()) {
+      setState(() {
+        _autoValidate = true;
+      });
+    }else{
+      FirebaseAuth.instance.currentUser().then((user) {
+      Firestore.instance.collection('Tasks').document().setData({
+        'body': _taskController.text,
+        'done': 'false',
+        'uid': user.uid,
+        'date' : DateTime.now() ,
+      }).then((_) {
+        Navigator.pop(context);
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => HomeScreen()));
+        Navigator.pop(context);
+      });
+    });
+    }
+    
+  }
+}
